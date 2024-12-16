bf7ad6b40b6650129a60ca2b1a79239b93b6b65e
davidmizra17 <davidmizrago@gmail.com>
2024-12-16 12:30:03 -0400

    feat: how to raise a 404 exception using a view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from polls.models import Question
 
@@ -10,14 +9,16 @@ from polls.models import Question
 def index(request):
     #get the last 5 questions added to the db
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s. " %question_id)
-
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("This question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 def results(request, question_id):
     response =  "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
